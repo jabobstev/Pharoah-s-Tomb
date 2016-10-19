f1bbf5b92c4d6895692880703acd9f15cb560886
denis.kefallinos <denis.kefallinos@silentpartnersoftware.com>
2016-10-19 12:50:09 -0400

    Tower and player horizontal rotation
    Player orbits around tower

---
+++ PharaohTomb_Onslaught/Assets/Scripts/TowerBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerBehavior : MonoBehaviour {
+
+    public int health;
+    public int charge;
+
+    // Use this for initialization
+    void Start () {
+        health = 100;
+        charge = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateAnimations();
+        BuildCharge();
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        //TODO
+        if (col.gameObject.name == "mummy?")
+        {
+            //take damage
+        }
+    }
+
+    void BuildCharge()
+    {
+        //TODO
+    }
+
+    void UpdateAnimations()
+    {
+        //TODO
+        //handles all animator variable
+    }
+}
