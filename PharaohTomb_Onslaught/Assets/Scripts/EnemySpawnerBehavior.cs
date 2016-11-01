e8752818c640d8eac9e5151a90bef9b626bca37d
jabob <jacob.desrochers1@gmail.com>
2016-11-01 14:30:18 -0400

    Enemy spawning
    added enemy spawner game object and spawn them around a circle

---
+++ PharaohTomb_Onslaught/Assets/Scripts/EnemySpawnerBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerBehavior : MonoBehaviour {
+
+    float SpawnRate = 6f;
+    public GameObject Enemy;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("SpawnEnemy", 4f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnEnemy()
+    {
+        float EnemyY = -0.25f;
+        float radius = 10f;
+        float angle = Random.value * Mathf.PI * 2;
+        Vector3 pos = new Vector3(Mathf.Cos(angle) * radius, EnemyY, Mathf.Sin(angle) * radius);
+        GameObject enemy = (GameObject)Instantiate(Enemy);
+        enemy.transform.position = pos;
+
+
+        if (SpawnRate - 0.5 >= 1)
+        {
+            SpawnRate -= 0.5f;
+        }
+        NextEnemySpawn(SpawnRate);
+    }
+
+    void NextEnemySpawn(float rate)
+    {
+        Invoke("SpawnEnemy", rate);
+    }
+}
