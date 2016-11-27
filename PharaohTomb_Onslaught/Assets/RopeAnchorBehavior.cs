4dbd13f8e90ff4ae221b974ccf43325fa23fa011
Denis <deniskefallinos94@gmail.com>
2016-11-27 16:20:47 -0500

    Cool ass rope

---
+++ PharaohTomb_Onslaught/Assets/RopeAnchorBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeAnchorBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(-0f, 1.51f, 14.76f);
+        transform.rotation = Quaternion.Euler(0,0,0);
+    }
+}
---
+++ PharaohTomb_Onslaught/Assets/RopeBheavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeBheavior : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z);
+	}
+}
